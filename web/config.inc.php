8671a918f19d16064549d8db664169b126333ce5
Tuan Nguyen <amtuannguyen@gmail.com>
2014-06-26 12:21:21 -0400

    readme

--- web/config.inc.php
+++ web/config.inc.php
@@ -51,15 +51,15 @@ $db_host = "localhost";
 // can uncomment the following line and specify the port number
 // $db_port = 1234;
 // Database name:
-$db_database = "__CHANGE_THIS__";
+$db_database = "mrbs";
 // Schema name.  This only applies to PostgreSQL and is only necessary if you have more
 // than one schema in your database and also you are using the same MRBS table names in
 // multiple schemas.
 //$db_schema = "public";
 // Database login user name:
-$db_login = "__CHANGE_THIS__";
+$db_login = "mrbs";
 // Database login password:
-$db_password = '__CHANGE_THIS__';
+$db_password = "password";
 // Prefix for table names.  This will allow multiple installations where only
 // one database is available
 $db_tbl_prefix = "mrbs_";
