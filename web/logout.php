575e1bb018f543503c6a3a641edc7a86b384d6d0
Tuan Nguyen <amtuannguyen@gmail.com>
2014-06-27 10:32:27 -0400

    more session

--- web/logout.php
+++ web/logout.php
@@ -1,7 +1,8 @@
 <?php
+require "defaultincludes.inc";
+
 ob_start();
 unset($_SESSION['REMOTE_USER']);
-session_destroy();
 setcookie('pyauth', '', time()-(3600 * 24 * 365), '/', 'yorku.ca');
 setcookie('mayaauth', '', time()-(3600 * 24 * 365), '/', 'yorku.ca');
 header('Location: index.php');
