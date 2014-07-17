6360d884ca1a21f2095827ea4822e4e434f37ac7
Tuan Nguyen <amtuannguyen@gmail.com>
2014-07-17 12:00:50 -0400

    fixed missing WHERE  in sql

--- web/york_functions.php
+++ web/york_functions.php
@@ -1,43 +1,43 @@
 <?php
 function override_area_hours($area) {
-	global $morningstarts, $morningstarts_minutes, $eveningends, $eveningends_minutes, $resolution;
-	global $day, $month, $year;
+    global $morningstarts, $morningstarts_minutes, $eveningends, $eveningends_minutes, $resolution;
+    global $day, $month, $year;
   
- 	// calculate selected day of the week
+     // calculate selected day of the week
     if (!$day || !$month || !$year) {
-	    $dow = date('N', mktime(0,0,0,date('n'),date('j'),date('Y')));
-	} else {
-		$dow = date('N', mktime(0,0,0,$month,$day,$year));
-	}
+        $dow = date('N', mktime(0,0,0,date('n'),date('j'),date('Y')));
+    } else {
+        $dow = date('N', mktime(0,0,0,$month,$day,$year));
+    }
 
     if (strrpos($_SERVER['REQUEST_URI'], 'week.php') !== false) {
-		$sql = "select min(morningstarts) as morningstarts, max(eveningends) as eveningends from mrbs_area_hours where morningstarts>0  and area_id=$area";
-		$res = sql_query($sql);
+        $sql = "select min(morningstarts) as morningstarts, max(eveningends) as eveningends from mrbs_area_hours where morningstarts>0  and area_id=$area";
+        $res = sql_query($sql);
         if ($res) {
-		    $row = sql_row_keyed($res, 0);
-		    $morningstarts = $row['morningstarts'];
-		    $morningstarts_minutes = 0;
-			$eveningends = $row['eveningends'];
-			$eveningends_minutes = 59;
-		}
-	} else {
-		// get the hours the selected day of the week            
-	    $sql = "SELECT * FROM mrbs_area_hours WHERE area_id=$area AND dayoftheweek=$dow LIMIT 1";
-	    $res = sql_query($sql);
-	    if ($res) {
-		    $row = sql_row_keyed($res, 0);
-		    $morningstarts = $row['morningstarts'];
-		    $morningstarts_minutes = $row['morningstarts_minutes'];
-			$eveningends = $row['eveningends'];
-			$eveningends_minutes = $row['eveningends_minutes'];
-		}
-		if ($morningstarts == 0 && $morningstarts_minutes == 0 
-		    && $eveningends == 0 && $eveningends_minutes == 0) {
-			$resolution = 1000000 * SECONDS_PER_DAY;
-			global $area_is_closed;
-			$area_is_closed = true;
-		}
-	}
+            $row = sql_row_keyed($res, 0);
+            $morningstarts = $row['morningstarts'];
+            $morningstarts_minutes = 0;
+            $eveningends = $row['eveningends'];
+            $eveningends_minutes = 59;
+        }
+    } else {
+        // get the hours the selected day of the week            
+        $sql = "SELECT * FROM mrbs_area_hours WHERE area_id=$area AND dayoftheweek=$dow LIMIT 1";
+        $res = sql_query($sql);
+        if ($res) {
+            $row = sql_row_keyed($res, 0);
+            $morningstarts = $row['morningstarts'];
+            $morningstarts_minutes = $row['morningstarts_minutes'];
+            $eveningends = $row['eveningends'];
+            $eveningends_minutes = $row['eveningends_minutes'];
+        }
+        if ($morningstarts == 0 && $morningstarts_minutes == 0 
+            && $eveningends == 0 && $eveningends_minutes == 0) {
+            $resolution = 1000000 * SECONDS_PER_DAY;
+            global $area_is_closed;
+            $area_is_closed = true;
+        }
+    }
 }
 
 function get_user_group() {
@@ -45,11 +45,11 @@ function get_user_group() {
 }
 
 function get_area_name($area) {
-    $sql = "select area_name from mrbs_area id=$area";
-	$res = sql_query($sql);
-	if ($res) {
-	    $row = sql_row_keyed($res, 0);
-	    return $row['area_name'];
+    $sql = "select area_name from mrbs_area where id=$area";
+    $res = sql_query($sql);
+    if ($res) {
+        $row = sql_row_keyed($res, 0);
+        return $row['area_name'];
     }
     return null;
 }
