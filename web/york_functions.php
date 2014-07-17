2b845e988423d54fecd563ec911098bfde4d9e3b
Tuan Nguyen <amtuannguyen@gmail.com>
2014-07-17 10:45:16 -0400

    correct indentation

--- web/york_functions.php
+++ web/york_functions.php
@@ -4,30 +4,30 @@ function override_area_hours($area) {
 	global $day, $month, $year;
   
  	// calculate selected day of the week
-  if (!$day || !$month || !$year) {
-	  $dow = date('N', mktime(0,0,0,date('n'),date('j'),date('Y')));
+    if (!$day || !$month || !$year) {
+	    $dow = date('N', mktime(0,0,0,date('n'),date('j'),date('Y')));
 	} else {
 		$dow = date('N', mktime(0,0,0,$month,$day,$year));
 	}
 
-  if (strrpos($_SERVER['REQUEST_URI'], 'week.php') !== false) {
+    if (strrpos($_SERVER['REQUEST_URI'], 'week.php') !== false) {
 		$sql = "select min(morningstarts) as morningstarts, max(eveningends) as eveningends from mrbs_area_hours where morningstarts>0  and area_id=$area";
 		$res = sql_query($sql);
-	  if ($res) {
-		  $row = sql_row_keyed($res, 0);
-		  $morningstarts = $row['morningstarts'];
-		  $morningstarts_minutes = 0;
+        if ($res) {
+		    $row = sql_row_keyed($res, 0);
+		    $morningstarts = $row['morningstarts'];
+		    $morningstarts_minutes = 0;
 			$eveningends = $row['eveningends'];
 			$eveningends_minutes = 59;
 		}
 	} else {
 		// get the hours the selected day of the week            
-	  $sql = "SELECT * FROM mrbs_area_hours WHERE area_id=$area AND dayoftheweek=$dow LIMIT 1";
-	  $res = sql_query($sql);
-	  if ($res) {
-		  $row = sql_row_keyed($res, 0);
-		  $morningstarts = $row['morningstarts'];
-		  $morningstarts_minutes = $row['morningstarts_minutes'];
+	    $sql = "SELECT * FROM mrbs_area_hours WHERE area_id=$area AND dayoftheweek=$dow LIMIT 1";
+	    $res = sql_query($sql);
+	    if ($res) {
+		    $row = sql_row_keyed($res, 0);
+		    $morningstarts = $row['morningstarts'];
+		    $morningstarts_minutes = $row['morningstarts_minutes'];
 			$eveningends = $row['eveningends'];
 			$eveningends_minutes = $row['eveningends_minutes'];
 		}
