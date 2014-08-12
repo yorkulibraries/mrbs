594a33092f0d63c35b0923c9a6f358682667d17c
Tuan Nguyen <amtuannguyen@gmail.com>
2014-08-12 16:10:28 -0400

    fixed alert-success typo

--- web/logout.php
+++ web/logout.php
@@ -8,7 +8,7 @@ setcookie('mayaauth', '', time()-(3600 * 24 * 365), '/', 'yorku.ca');
 ?>
 <?php print_header($day, $month, $year, $area, isset($room) ? $room : ""); ?>
 <h1><?php echo get_vocab("logged_out")?></h1>
-<div class="alert alert-successs" role="alert">
+<div class="alert alert-success" role="alert">
   <?php echo get_vocab("logged_out_success")?>
 </div>
 <?php print_footer(TRUE); ?>
